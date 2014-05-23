68e8764cebf459c281d73dee737a5d7e28473a81
Alive2212 <Alive2212@yahoo.com>
2014-05-23 16:52:08 +0430

    RFT230 Base
    Add checking RFT230 Connection and remove last Buffer

--- Dot Net/CS/Class/RFID/RFT230/RFT230_Main-v1.0.0.0.cs
+++ Dot Net/CS/Class/RFID/RFT230/RFT230_Main-v1.0.0.0.cs
@@ -13,34 +13,52 @@ namespace RFT230
         public static string oldsn;
 
         public static void RFT230_INIT()
-        {            
+        {
             RFT230.API.MF_InitComm("USB", 9600);
+        }
+
+        public static bool IsRFT230Connect()
+        {
+            bool Temp = true;
+            if (RFT230.API.MF_Anticoll(0, ref RFID.Mifare.cardSN[0]) == 33)
+            {
+                Temp = false;
+            }
+            return Temp;
         }
         public static string rft230_GetID()
         {
             string Temp = "";
-            int a = RFT230.API.MF_Anticoll(0, ref RFID.Mifare.cardSN[0]);
+            string x = "N/A";
 
-            RFT230.API.MF_Request(0, 0, ref RFID.Mifare.cardT[0]);
-            for (int i = 0; i <= 2; i++)
+            if (IsRFT230Connect())
             {
-                Temp += Convert.ToString(RFID.Mifare.cardT[i]);
-            }
-
-            string x = "";
-            for (int i = 0; i < 4; i++)
-            {
-                x += string.Format("{0:X}", RFID.Mifare.cardSN[i]);
-            }
-            if (a == 0)
-            {
-                if (oldsn != x)
+                x = "";
+                int a = RFT230.API.MF_Anticoll(0, ref RFID.Mifare.cardSN[0]);
+                RFT230.API.MF_Request(0, 0, ref RFID.Mifare.cardT[0]);
+                for (int i = 0; i <= 2; i++)
                 {
-                    oldsn = x;
+                    Temp += Convert.ToString(RFID.Mifare.cardT[i]);
+                }
+
+                for (int i = 0; i < 4; i++)
+                {
+                    x += string.Format("{0:X}", RFID.Mifare.cardSN[i]);
+                }
+                for (int j = 0; j < RFID.Mifare.cardSN.Length; j++)
+                    RFID.Mifare.cardSN[j] = 0;
+
+                if (a == 0)
+                {
+                    if (oldsn != x)
+                    {
+                        oldsn = x;
+                    }
                 }
             }
             return x;
         }
     }
 }
+
 
